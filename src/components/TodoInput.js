2faad65bdbcedd277c6792d3195524147b93bd68
hamzaali <hamza54834@gmail.com>
2020-06-12 13:43:39 -0700

    setup complete

---
+++ src/components/TodoInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+ 
+class TodoInput extends Component {
+    render() { 
+        return (
+            <div>
+            Hello From TodoInput
+            </div>
+        );
+    }
+}
+ 
+export default TodoInput;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Item from './TodoItem';
+
+class TodoItem extends Component {
+    render() { 
+        return (
+            <div>
+                Hello World TodoItem
+            </div>
+        );
+    }
+}
+ 
+export default TodoItem;
